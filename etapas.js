1189fa67e326fee45d65ef71ffc0b20b728e84fe
Anderson Ribas <ander.ribas@gmail.com>
2021-12-29 18:17:22 -0300

    Primeiro commit

---
+++ etapas.js
@@ -0,0 +1,68 @@
+let etapas = 
+[
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: 
+        [
+            {
+                numero: '38111',
+                nome: 'Chewbacca',
+                partido: 'PAM',
+                fotos:
+                [
+                    {url:'chewbacca.jfif', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero:'77222',
+                nome:'Darth Vader',
+                partido: 'ABCD',
+                fotos:
+                [
+                    {url:'dv.jfif', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero:'32000',
+                nome:'Jabba',
+                partido: 'RWQ',
+                fotos:
+                [
+                    {url:'jabba.jfif', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:
+        [
+            {
+                numero: '99',
+                nome: 'Mestre dos Magos',
+                partido:'MM',
+                vice:'Dumbledore',
+                fotos:
+                [
+                    {url:'mm.jfif', legenda:'Prefeito'},
+                    {url:'dumbledore.jfif', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero:'84',
+                nome: 'Mestre Yoda',
+                partido:'MY',
+                vice: 'Gandalf',
+                fotos:
+                [
+                    {url: 'my.jfif', legenda: 'Prefito'},
+                    {url: 'gandalf.jfif', legenda: 'Vice-Prefito', small: true}
+
+                ]
+            },
+        ]
+    }
+
+];
